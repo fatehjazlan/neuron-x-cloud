e68c83ce7e171fb81e27b4a9b432d1892fa51681
fatehjazlan <fatehbossku@gmail.com>
2026-09-25 22:58:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ Return ONLY the category name.
 
     url = (
         "https://generativelanguage.googleapis.com/"
-        "v1beta/models/gemini-2.5-flash:generateContent"
+        "v1beta/models/gemini-3.5-flash:generateContent"
     )
 
     payload = {
